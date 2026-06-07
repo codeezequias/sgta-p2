207d38d4e271f2437a5c87d378c01ee3ba88d8d9
Lucas Ezequias <lucaeze007@gmail.com>
2026-06-07 20:19:28 -0300

    Alteracoes

---
+++ backend/tarefas/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Tarefa(models.Model):
+    STATUS_CHOICES = [
+        ('ABERTA', 'Aberta'),
+        ('EM_ANDAMENTO', 'Em andamento'),
+        ('CONCLUIDA', 'Concluida'),
+        ('CANCELADA', 'Cancelada'),
+    ]
+
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES , default='ABERTA')
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_entrega = models.DateField()
+
+    def __str__(self):
+        return self.titulo
